0fe2b27d2bbdf8ca093c17585c5a1bc04a757a15
LeoRebelatto <leonardorebelatto@hotmail.com>
2020-11-06 16:52:42 -0300

    route img

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 
 app.get('/', (function(req,res) {
     res.sendFile(__dirname + '/smultimidia.html');
-})); 
+}));
+
+app.use(express.static('img'));
 
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'));
