6bf7211d8d1ec10a5ba1bbda910df80a4f907a5a
LeoRebelatto <leonardorebelatto@hotmail.com>
2020-11-06 17:39:23 -0300

    sadfg

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use('/img', express.static('img'));
 
 function requireHTTPS(req, res, next) {
     // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    if (req.get('x-forwarded-proto') !== 'https') {
       return res.redirect('https://' + req.get('host') + req.url);
     }
     next();
