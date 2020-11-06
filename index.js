824b01d03018ae6d866775d1c9754ed077b9f0b0
LeoRebelatto <leonardorebelatto@hotmail.com>
2020-11-06 17:44:23 -0300

    correção https

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express'); 
 
+var secure = require('express-force-https');
+
 const app = express(); 
 
 app.get('/', (function(req,res) {
@@ -8,14 +10,7 @@ app.get('/', (function(req,res) {
 
 app.use('/img', express.static('img'));
 
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (req.get('x-forwarded-proto') !== 'https') {
-      return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-  }
-  app.use(requireHTTPS);
+app.use(secure);
 
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'));
