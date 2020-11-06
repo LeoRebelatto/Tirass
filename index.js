a4b66ea90815158a34728d966f0ada541c47c87c
LeoRebelatto <leonardorebelatto@hotmail.com>
2020-11-06 17:33:25 -0300

    HTTPS

--- index.js
+++ index.js
@@ -8,5 +8,14 @@ app.get('/', (function(req,res) {
 
 app.use('/img', express.static('img'));
 
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+      return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+  }
+  app.use(requireHTTPS);
+
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'));
